3b27d987e54c96d041b3b45c1ad42f544c9cbbf2
mili Arsema <meleymm88@gmail.com>
2026-04-17 01:07:38 -0500

    added comment

--- src/main/java/com/pluralsight/HelloWorld.java
+++ src/main/java/com/pluralsight/HelloWorld.java
@@ -2,6 +2,7 @@ package com.pluralsight;
 
 public class HelloWorld {
     public static void main(String[] args) {
+        //practicing w print line code
         System.out.println("Hello World!");
         System.out.println("Hello Universe!");
 
